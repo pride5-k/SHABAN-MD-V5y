28be69a7757246234f09bf4c904ad4cf71bc3abb
Ptkhacker <pridekusvabadika@gmail.com>
2025-07-20 16:47:30 +0200

    Update config.js
    Signed-off-by: Ptkhacker  <pridekusvabadika@gmail.com>

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "SHABAN-MD~EEIiyQqQ#AScrgBnwtfLMahyf-JSa33qvQlzzvr_FTVVCM54q9W4",
 // add your Session Id
 AUTO_BIO: process.env.AUTO_BIO || "false", 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
